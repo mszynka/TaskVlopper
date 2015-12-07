f65c22b2cbf159b1765b46a32d72f1f9197ce4a6
Mateusz Szynka <devmsw12@gmail.com>
2015-12-07 09:01:12 +0100

    Logger fix
    Fixes #43

--- TaskVlopper.Repository/Base/Logger.cs
+++ TaskVlopper.Repository/Base/Logger.cs
@@ -10,19 +10,79 @@ namespace TaskVlopper.Repository.Base
 {
     public static class Logger
     {
+        [AttributeUsage(AttributeTargets.Field)]
+        public class ReprAttribute : Attribute
+        {
+            public string Representation;
+            public ReprAttribute(string representation)
+            {
+                this.Representation = representation;
+            }
+            public override string ToString()
+            {
+                return this.Representation;
+            }
+        }
+
+
+        public enum LoggerSeverityEnum
+        {
+            [Repr("EXCEPTION")]
+            Exception,
+
+            [Repr("WARNING")]
+            Warning,
+
+            [Repr("INFO")]
+            Info,
+
+            [Repr("LOG")]
+            Log
+        }
+
+        public static void Log(string message, LoggerSeverityEnum severity = LoggerSeverityEnum.Log)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter(HttpContext.Current.Server.MapPath("~\\Logger\\" + DateTime.Now.ToShortDateString().ToString().Replace("/", "_") + ".txt"), true))
+                {
+                    file.WriteLine("[" + severity.ToString() + "] " + DateTime.Now.ToString());
+                    file.WriteLine(message + "\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!Directory.Exists(@"C:\Temp\Logger"))
+                {
+                    Directory.CreateDirectory(@"C:\Temp\Logger");
+                }
+
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(string.Join("", "C:\\Temp\\Logger\\", DateTime.Now.ToShortDateString().ToString().Replace("/", "_") + ".txt"), true))
+                {
+                    file.WriteLine("[" + severity.ToString() + "] " + DateTime.Now.ToString());
+                    file.WriteLine(message + "\n");
+                    file.WriteLine("[EXCEPTION] While handling exception another occured!");
+                    file.WriteLine(ex.Message.ToString() + "\n");
+
+                }
+            }
+        }
+
         public static void LogException(string message)
         {
-            //if (!Directory.Exists(@"C:\TaskVlopper"))
-            //{
-            //    Directory.CreateDirectory(@"C:\TaskVlopper");
-            //}
-            
-            using (System.IO.StreamWriter file = 
-                new System.IO.StreamWriter(HttpContext.Current.Server.MapPath("~\\Logger_" + DateTime.Now.ToShortDateString() + ".txt"), true))
-            {
-                file.WriteLine("[EXCEPTION] " +DateTime.Now.ToString());
-                file.WriteLine(message + "\n");
-            }
+            Logger.Log(message, LoggerSeverityEnum.Exception);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Logger.Log(message, LoggerSeverityEnum.Warning);
+        }
+
+        public static void LogInfo(string message)
+        {
+            Logger.Log(message, LoggerSeverityEnum.Info);
         }
     }
 }
