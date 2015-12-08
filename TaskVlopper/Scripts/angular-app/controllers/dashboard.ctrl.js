d0267381d148492bf98d51e74df96bcd50ae3e5f
Mateusz Szynka <devmsw12@gmail.com>
2015-12-08 10:48:34 +0100

    Fluid containers, angular dashboard management
    TODO: Ng postForProject action

--- TaskVlopper/Scripts/angular-app/controllers/dashboard.ctrl.js
+++ TaskVlopper/Scripts/angular-app/controllers/dashboard.ctrl.js
@@ -4,16 +4,11 @@
     function getProjects() {
         ProjectService.getProjects().success(function (response) {
             $scope.projects = response.Projects;
-            console.log($scope.projects);
         })
         .error(function (error) {
             $scope.status = 'Unable to load data: ' + error.message;
             console.log($scope.status);
         });
-    }
-
-    $scope.hasProjects = function() {
-        return $scope.projects.length > 0 ? false : true;
     }
 
 });
