c5f41b91460da3b8546dcfd77a382cc44a64fd4a
Mateusz Szynka <devmsw12@gmail.com>
2015-12-14 11:05:01 +0100

    Directives, promises, css fix

--- TaskVlopper/Scripts/angular-app/controllers/project.ctrl.js
+++ TaskVlopper/Scripts/angular-app/controllers/project.ctrl.js
@@ -3,46 +3,22 @@
     $scope.currentProjectId = $stateParams.projectId;
     $scope.projectHandler = {};
     $scope.projectHandler.getProjects = function () {
-        ProjectService.getAll()
-            .success(function (response) {
-                if (response.HttpCode != undefined) {
-                    console.log(response.HttpCode + " " + response.Message);
-                }
-                $scope.projects = response.Projects;
-            })
-        .error(function (error) {
-            $scope.status = '[ProjectService.getAll] Unable to load data: ' + error.message;
-            console.log($scope.status);
-        });
+        ProjectService.getAll().then(function (response) {
+            $scope.projects = response;
+        })
     }
     $scope.projectHandler.getProjects();
 
     $scope.projectHandler.getProject = function (projectId) {
-        ProjectService.get(projectId)
-        .success(function (response) {
-            if (response.HttpCode != undefined) {
-                console.log(response.HttpCode + " " + response.Message);
-            }
-            $scope.model = response.Project;
+        ProjectService.get(projectId).then(function (response) {
+            $scope.model = response;
         })
-        .error(function (error) {
-            $scope.status = '[ProjectService.get] Unable to load data: ' + error.message;
-            console.log($scope.status);
-        });
     };
 
     $scope.projectHandler.createProject = function () {
-        ProjectService.create($scope.model)
-        .success(function (response) {
-            if (response.HttpCode != undefined) {
-                console.log(response.HttpCode + " " + response.Message);
-            }
+        ProjectService.create($scope.model).then(function(response){
             $scope.projectHandler.getProjects();
         })
-        .error(function (error) {
-            $scope.status = '[ProjecService.create] Unable to load data: ' + error.message;
-            console.log($scope.status);
-        });
     };
 
     $scope.projectHandler.initEditor = function () {
@@ -52,48 +28,15 @@
     $scope.projectHandler.editProject = function () {
         var temp_model = $scope.model;
         delete temp_model['$$hashkey'];
-        console.log(temp_model);
-        ProjectService.update(temp_model)
-        .success(function (response) {
-            if (response.HttpCode != undefined) {
-                console.log(response.HttpCode + " " + response.Message);
-            }
+        ProjectService.update(temp_model).then(function(response){
             $scope.projectHandler.getProjects();
-        })
-        .error(function (error) {
-            $scope.status = '[ProjectService.deleteProject] Unable to load data: ' + error.message;
-            console.log($scope.status);
         });
     };
 
     $scope.projectHandler.deleteProject = function () {
-        ProjectService.delete($scope.currentProjectId)
-        .success(function (response) {
-            if (response.HttpCode != undefined) {
-                console.log(response.HttpCode + " " + response.Message);
-            }
+        ProjectService.delete($scope.currentProjectId).then(function (response) {
             $scope.model = null;
             $scope.projectHandler.getProjects();
-        })
-        .error(function (error) {
-            $scope.status = '[ProjectService.deleteProject] Unable to load data: ' + error.message;
-            console.log($scope.status);
         });
     }
 });
-
-app
-    .directive('tvProjectViewer', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: "static/templates/project/viewer.html"
-        }
-    })
-    .directive('tvProjectModeleditor', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: "static/templates/project/modeleditor.html"
-        }
-    });
--- TaskVlopper/Scripts/angular-app/services/project.service.js
+++ TaskVlopper/Scripts/angular-app/services/project.service.js
@@ -1,27 +1,58 @@
 ﻿app.service('ProjectService', ['$http', function ($http) {
 
         this.getAll = function () {
-            return $http.get('/Project');
+            return $http.get('/Project')
+            .then(function (response) {
+                if (response.data.HttpCode != undefined) {
+                    console.log(response.data.HttpCode + " " + response.data.Message);
+                }
+                return response.data.Projects;
+            })
+            .catch(function (error) {
+                $scope.status = '[ProjectService.getAll] Unable to load data: ' + error.data.message;
+                console.log($scope.status);
+            });
         };
 
         this.get = function (projectId) {
             if (projectId !== undefined || !isNaN(projectId)) {
-                return $http.get('/Project/Details/' + projectId);
+                return $http.get('/Project/Details/' + projectId)
+                .then(function (response) {
+                    if (response.data.HttpCode != undefined) {
+                        console.log(response.data.HttpCode + " " + response.data.Message);
+                    }
+                    return response.data.Project;
+                })
+                .catch(function (error) {
+                    $scope.status = '[ProjectService.get] Unable to load data: ' + error.message;
+                    console.log($scope.status);
+                });
             }
             else {
                 $scope.status = "[ProjectService.get] ProjectID is invalid!";
                 console.log($scope.status);
+                return null;
             }
         };
 
         this.create = function (model) {
-            model = JSON.stringify(model);
+            var newmodel = JSON.stringify(model);
             return $http({
                 method: 'POST',
                 url: '/Project/Create',
                 accept: 'application/json',
-                data: model
+                data: newmodel
             })
+            .then(function (response) {
+                if (response.data.HttpCode != undefined) {
+                    console.log(response.data.HttpCode + " " + response.data.Message);
+                }
+                return response;
+            })
+            .catch(function (error) {
+                $scope.status = '[ProjectService.create] Unable to load data: ' + error.message;
+                console.log($scope.status);
+            });
         };
 
         this.update = function (model) {
@@ -30,16 +61,37 @@
                 url: '/Project/Edit/' + model.ID,
                 accept: 'application/json',
                 data: model
+            })
+            .then(function (response) {
+                if (response.data.HttpCode != undefined) {
+                    console.log(response.data.HttpCode + " " + response.data.Message);
+                }
+                return response;
+            })
+            .catch(function (error) {
+                $scope.status = '[ProjectService.update] Unable to load data: ' + error.message;
+                console.log($scope.status);
             });
         }
 
         this.delete = function (projectId) {
             if (projectId !== undefined || !isNaN(projectId))
-                return $http.post('/Project/Delete/' + projectId);
+                return $http.post('/Project/Delete/' + projectId)
+                .then(function (response) {
+                    if (response.data.HttpCode != undefined) {
+                        console.log(response.data.HttpCode + " " + response.data.Message);
+                    }
+                    return response;
+                })
+                .catch(function (error) {
+                    $scope.status = '[ProjectService.delete] Unable to load data: ' + error.message;
+                    console.log($scope.status);
+                });
             
             else {
                 $scope.status = "[ProjectService.delete] ProjectID is invalid!";
                 console.log($scope.status);
+                return null;
             }
         };
 
