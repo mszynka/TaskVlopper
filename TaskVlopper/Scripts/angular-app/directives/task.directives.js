0ac4399edf4f7e452aa6e3dcde1bc1728d459935
Mateusz Szynka <devmsw12@gmail.com>
2015-12-22 22:22:48 +0100

    Css and project magic, working tasks

--- TaskVlopper/Scripts/angular-app/directives/task.directives.js
+++ TaskVlopper/Scripts/angular-app/directives/task.directives.js
@@ -1,11 +1,4 @@
 ﻿app
-    .directive('tvTaskViewer', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: "static/templates/task/viewer.html"
-        }
-    })
     .directive('tvTaskModeleditor', function () {
         return {
             restrict: 'E',
